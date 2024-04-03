fa2bec833a8c03c869d29c857f6b03e84820dfc4
Sean Breen <s.breen14@nuigalway.ie>
2024-04-03 14:05:59 +0100

    work on tflite interpretation

--- mini-project-work.py
+++ mini-project-work.py
@@ -126,6 +126,6 @@ for i in range(len(scores)):
 # postprocess_frame(frame, boxes, scores, classes)
 
 # Postprocess and display the frame
-frame = postprocess_frame(frame, output_data)
+frame = postprocess_frame(frame, output_data, scores, classes)
 cv2.imwrite('output.jpg', frame)
 # cv2.imshow('Object Detection', frame)
