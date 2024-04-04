457e12d1e3ea86e135ff9b62ea58a1d3dbec5cc8
Sean Breen <s.breen14@nuigalway.ie>
2024-04-04 12:51:22 +0100

    Picture inference complete

--- detectonpicture.py
+++ detectonpicture.py
@@ -11,44 +11,12 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-print('input',input_details)
-print('output', output_details)
-
-
-
 # Define a function to preprocess the frame
 def preprocess_frame(frame):
     # Adjust these preprocessing steps as per your model requirements
     frame_resized = cv2.resize(frame, (input_details[0]['shape'][2], input_details[0]['shape'][1]))
     frame_normalized = frame_resized / 255.0  # Normalize if your model expects this
     return np.expand_dims(frame_normalized, axis=0).astype(np.float32)
-
-# def draw_detection(frame, box, color=(255, 0, 0), thickness=2):
-#     """Draw a single detection box on the frame."""
-#     height, width = frame.shape[:2]
-#     ymin, xmin, ymax, xmax = box
-#     start_point = (int(xmin * width), int(ymin * height))
-#     end_point = (int(xmax * width), int(ymax * height))
-#     frame = cv2.rectangle(frame, start_point, end_point, color, thickness)
-#     return frame
-
-# def postprocess_frame(frame, boxes, scores, classes, threshold=0.2):
-#     # boxes: Bounding box coordinates of detected objects
-#     # scores: Confidence of detected objects
-#     # classes: Class index of detected objects
-#     for i in range(len(scores)):
-#         if scores[i] > threshold:
-#             box = boxes[i]  # y_min, x_min, y_max, x_max
-#             class_id = int(classes[i])
-#             confidence = scores[i]
-#             label = labels[class_id] if class_id < len(labels) else 'Unknown'
-#             frame = draw_detection(frame, box)
-#             # Optionally, add text for label and confidence score
-#             label_text = f'{label}: {confidence:.2f}'
-#             start_point = (int(box[1] * frame.shape[1]), int(box[0] * frame.shape[0]))  # x_min, y_min
-#             cv2.putText(frame, label_text, start_point, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-#     return frame
-
 
 def classFilter(classdata):
     classes = []  # create a list
@@ -70,18 +38,12 @@ def YOLOdetect(output_data):  # input = interpreter, output is boxes(xyxy), clas
 
 frame = cv2.imread('testImage.jpg')
 frame = cv2.resize(frame, (640, 640))
-#cv2.imshow('frame', frame)
-
-#H, W = frame.shape[:2]  # Get frame height and width
-
 
 copyFrame = frame.copy()
 copyFrame = cv2.resize(copyFrame, (640, 640))
 # Preprocess the frame
 input_data = preprocess_frame(copyFrame)
 interpreter.set_tensor(input_details[0]['index'], input_data)
-
-# cv2.imshow('preprocessed frame', input_data)
 
 # Run inference
 interpreter.invoke()
@@ -126,22 +88,6 @@ for i in indices:
     formatted_confidence = "{:.2f}".format(confidences[i])
     cv2.putText(frame, formatted_confidence, (x + 100, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 2)
 
-
-
-# Print output details for debugging
-# for i, detail in enumerate(output_details):
-#     print(f"Output {i}: {detail}")
-
-# Make sure the length of output_data is as expected
-#print(f"Number of output tensors: {len(output_data)}")
-
-
-#boxes, classes, scores = output_data[boxes_idx], output_data[classes_idx], output_data[scores_idx]
-# postprocess_frame(frame, boxes, scores, classes)
-
-# Postprocess and display the frame
-# frame = postprocess_frame(frame, output_data, scores, classes)
-#cv2.imwrite('sean.jpg', frame)
 cv2.namedWindow('detect_result', cv2.WINDOW_NORMAL)
 cv2.imshow('detect_result', frame)
 cv2.waitKey(0)
