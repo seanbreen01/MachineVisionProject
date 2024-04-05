dded0f2bd2afa8fc8c53262ad2a3e1010fe1553e
Sean Breen <s.breen14@nuigalway.ie>
2024-04-05 20:45:45 +0100

    Cleanup for report

--- realTime.py
+++ realTime.py
@@ -24,18 +24,17 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 def preprocess_frame(frame):
-    # Adjust these preprocessing steps as per your model requirements
     frame_resized = cv2.resize(frame, (input_details[0]['shape'][2], input_details[0]['shape'][1]))
     frame_normalized = frame_resized / 255.0  # Normalize if your model expects this
     return np.expand_dims(frame_normalized, axis=0).astype(np.float32)
 
 def classFilter(classdata):
-    classes = []  # create a list
+    classes = [] 
     for i in range(classdata.shape[0]):         # loop through all predictions
         classes.append(classdata[i].argmax())   # get the best classification location
     return classes  # return classes (int)
 
-def YOLOdetect(output_data):  # input = interpreter, output is boxes(xyxy), classes, scores
+def detect(output_data):                    # input = interpreter, output is boxes(xyxy), classes, scores
     output_data = output_data[0]                # x(1, 25200, 7) to x(25200, 7)
     boxes = np.squeeze(output_data[..., :4])    # boxes  [25200, 4]
     scores = np.squeeze( output_data[..., 4:5]) # confidences  [25200, 1]
@@ -47,10 +46,7 @@ def YOLOdetect(output_data):  # input = interpreter, output is boxes(xyxy), clas
     return xyxy, classes, scores  # output is boxes(x,y,x,y), classes(int), scores(float) [predictions length]
 
 
-# Video file handling
-video_path = 'Inference Images/video.mp4'  # Specify the path to your video file
-
-pipeline = gstreamer_pipeline(flip_method=2)
+pipeline = gstreamer_pipeline(flip_method=0)
 cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
 
 while cap.isOpened():
@@ -70,13 +66,13 @@ while cap.isOpened():
 
     # Run inference
     output_data = interpreter.get_tensor(output_details[0]['index'])  # get tensor  x(1, 25200, 7)
-    xyxy, classes, scores = YOLOdetect(output_data) #boxes(x,y,x,y), classes(int), scores(float) [25200]
+    xyxy, classes, scores = detect(output_data) 
 
     # Convert xyxy to the format expected by OpenCV NMS and prepare scores
     boxes = []
     confidences = []
     for i in range(len(scores)):
-        if scores[i] > 0.75:
+        if scores[i] > 0.4:
             H = frame.shape[0]
             W = frame.shape[1]
             xmin = max(1, (xyxy[0][i] * W))
@@ -88,7 +84,7 @@ while cap.isOpened():
 
     # Apply Non-Maximum Suppression
     nms_threshold = 0.4  # NMS threshold, can be adjusted
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, score_threshold=0.75, nms_threshold=nms_threshold)
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, score_threshold=0.4, nms_threshold=nms_threshold)
 
     # Draw the rectangles and labels for NMS filtered detections
     for i in indices:
@@ -107,8 +103,6 @@ while cap.isOpened():
         cv2.putText(frame, labels[classes[i]], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 2)
         formatted_confidence = "{:.2f}".format(confidences[i])
         cv2.putText(frame, formatted_confidence, (x + 100, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (36, 255, 12), 2)
-
-  
 
     cv2.imshow('Frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
