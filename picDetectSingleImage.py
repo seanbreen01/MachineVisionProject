3ba0045cc411d9eacc91f9585de9f5c70d521fe1
Sean Breen <s.breen14@nuigalway.ie>
2024-04-05 21:10:04 +0100

    changes

--- picDetectSingleImage.py
+++ picDetectSingleImage.py
@@ -72,7 +72,7 @@ nms_threshold = 0.4  # NMS threshold, can be adjusted
 
 # Draw the rectangles and labels for NMS filtered detections
 for i in range(len(scores)):
-    i = i[0]  # Unpack the index
+    #i = i[0]  # Unpack the index
     box = boxes[i]
     x, y, w, h = box[0], box[1], box[2], box[3]
 
